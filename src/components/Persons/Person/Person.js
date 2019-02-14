b86265b76a90111edf48f9584b89590b1caf5f05
Cancain <eriksson.tomas@gmail.com>
2019-02-14 13:12:46 +0100

    completed lecture 108

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 import PropTypes from 'prop-types';
+import authContext from '../../../context/auth-context';
 
 class Person extends Component {
     constructor(props) {
@@ -17,7 +18,10 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (<Fragment>
-            {this.props.isAuth ? <p>Authenticated!</p> : <p>Please log in</p>}
+            <authContext.Consumer>
+                {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
+            </authContext.Consumer>
+            {}
             <p key="i1" onClick={this.props.click}>
                 I'm {this.props.name} and I am {this.props.age} years old!
             </p>
